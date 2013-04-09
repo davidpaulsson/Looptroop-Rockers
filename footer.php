bdf99b677a0a1f1a88edad08a35d1d690c848e27
David Paulsson <david@davidpaulsson.se>
2013-04-09 22:02:52 +0200

    Site footer in place.

--- footer.php
+++ footer.php
@@ -12,7 +12,45 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
+
+			<img src="<?php echo get_template_directory_uri() ?>/img/ltrlogo.png">
+
+			<div class="contact">
+				<h3>Contact</h3>
+				<p>Visit the contact page, <a href="https://twitter.com/looptroopdvsg/">hit us up on Twitter</a> or use one of the e-mail addresses listed here.</p>
 			
+				<h3>Bookings</h3>
+				<p><a href="mailto:ola@luger.se">Ola Broquist</a><br />
+				<a href="http://www.luger.se">www.luger.se</a><br />
+				+46 (0) 8-578 679 00</p>
+			</div>
+
+			<div class="press">
+				<h3>Press / Interviews Sweden</h3>
+				<p><a href="mailto:christian@skiva.se">Christian Östlund</a><br>
+				<a href="http://www.skiva.se">www.skiva.se</a></p>
+			
+				<h3>Press / Interviews outside Sweden</h3>
+				<p><a href="mailto:johan@badtasterecords.se">Johan Nordqvist</a></p>
+			
+				<h3>Tour questions / Management</h3>
+				<p><a href="mailto:perry@looptrooprockers.com">Perry Schumi</a></p>
+			</div>
+			
+			<div class="social">
+				<h3>Stay connected</h3>
+				<p><a href="http://www.looptrooprockers.com/feed/">Follow the RSS feed</a><br>
+				<a href="http://www.bloglovin.com/sv/blog/557880/looptroop-rockers">Follow us on BlogLovin'</a><br>
+				<a href="http://twitter.com/embeemusic">Embee on Twitter</a><br>
+				<a href="http://twitter.com/LooptroopDVSG">Looptroop Rockers on Twitter</a><br>
+				<a href="http://www.facebook.com/embeemusic">Embee on Facebook</a><br>
+				<a href="http://www.facebook.com/looptrooprockers">Looptroop Rockers on Facebook</a><br>
+				<a href="http://www.youtube.com/user/dvsgee">Looptroop Rockers on Youtube</a><br></p>
+			</div>
+
+			<hr>
+
+			<p>© Looptroop Rockers <?php echo date('Y') ?> | Site by <a href="http://davidpaulsson.se/">David Paulsson</a> | <a href="https://github.com/davidpaulsson/Looptroop-Rockers-v4/">Want to help out, nerd?</a></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
