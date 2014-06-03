e7a76c7e245928779065f6bc87f6519c8087552d
David Paulsson <david@davidpaulsson.se>
2014-06-03 09:22:38 +0200

    New press contact

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 			<div class="contact">
 				<h3>Contact</h3>
 				<p><a href="https://twitter.com/looptroopdvsg/" rel="me">Hit us up on Twitter</a> or use one of the e-mail addresses listed here.</p>
-			
+
 				<h3>Bookings</h3>
 				<p><a href="mailto:ola@luger.se">Ola Broquist</a><br />
 				<a href="http://www.luger.se">www.luger.se</a><br />
@@ -27,16 +27,16 @@
 
 			<div class="press">
 				<h3>Press</h3>
-				<p><a href="mailto:petra@looptrooprockers.com">Petra Åkesson</a></p>
+				<p><a href="mailto:amir.sela@sonymusic.com">Amir Sela</a></p>
 
 				<h3>Label inquires</h3>
 				<p><a href="mailto:claes@looptrooprockers.com">Claes Uggla</a><br />
 				<a href="mailto:jens@looptrooprockers.com">Jens Bengtsson</a></p>
-			
+
 				<h3>Tour questions / Management</h3>
 				<p><a href="mailto:perry@looptrooprockers.com">Perry Schumi</a></p>
 			</div>
-			
+
 			<div class="social">
 				<h3>Stay connected</h3>
 				<p><a href="http://www.looptrooprockers.com/feed/">Follow the RSS feed</a><br>
