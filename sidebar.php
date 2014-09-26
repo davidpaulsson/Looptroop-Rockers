0a1d9202e3aa1f56bee15cd53a2a783d08852324
David Paulsson <david@davidpaulsson.se>
2014-09-26 09:22:03 +0200

    Susy layout

--- sidebar.php
+++ sidebar.php
@@ -11,5 +11,15 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+  <div class="widget-area__yt">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore et, omnis, voluptate iste, pariatur officiis sunt, nam doloribus fuga corporis quia debitis dolores. Omnis quis labore tenetur tempore, provident dignissimos.
+  </div>
+  <div class="widget-area__one">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure quo odio eligendi, libero ducimus, doloremque, voluptate non hic voluptates accusamus facere ullam quasi accusantium. Error tempora velit suscipit id magnam.
+    <?php // dynamic_sidebar( 'sidebar-1' ); ?>
+  </div>
+  <div class="widget-area__two">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex beatae expedita, iste magnam reiciendis laudantium ab, distinctio blanditiis illo dolor odit, natus, asperiores laboriosam labore perspiciatis neque? Perferendis, est numquam!
+    <?php // dynamic_sidebar( 'sidebar-2' ); ?>
+  </div>
 </div><!-- #secondary -->
