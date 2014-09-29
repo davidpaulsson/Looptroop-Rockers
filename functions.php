b0c117a24e47e44a7859d701bfcef303b319dc14
David Paulsson <david@davidpaulsson.se>
2014-09-29 07:27:18 +0200

    Remove post formats

--- functions.php
+++ functions.php
@@ -57,9 +57,9 @@ function ltr_setup() {
 	 * Enable support for Post Formats.
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
-	) );
+	// add_theme_support( 'post-formats', array(
+	// 	'aside', 'image', 'video', 'quote', 'link',
+	// ) );
 
 	// Setup the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'ltr_custom_background_args', array(
