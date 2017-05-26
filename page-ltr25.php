21952f353238bd9f1f01e69de21d87a64713709b
David Paulsson <david@davidpaulsson.se>
2017-05-26 08:51:48 +0200

    Add Motivation Music

--- page-ltr25.php
+++ page-ltr25.php
@@ -103,14 +103,14 @@
                         </div>
                         <div class="content-right">
                             <h1>Check back every friday for a brand new release!</h1>
-                            <h2>Next release on May 26, 2017</h2>
+                            <h2>Next release on June 2, 2017</h2>
                         </div>
                     </header>
 
                     <div class="releases-list">
                         <div class="release release--unreleased">
                             <div class="release__img">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/04_LTR25_NextReleaseMAY26.jpg" alt="Available May 26">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/05_LTR25_NextReleaseJUNE2.jpg" alt="Available June 2">
                             </div>
                             <div class="release__col1">
                                 <div class="release__artist">Artist</div>
@@ -119,8 +119,30 @@
                             </div>
                             <div class="release__col2">
                                 <div class="release__id">DVSGXXX</div>
-                                <div class="release__date">May 26, 2017</div>
+                                <div class="release__date">June 2, 2017</div>
                                 <a href="" class="release__ext-link">
+                                    <svg viewBox="0 0 64 64">
+                                        <use xlink:href="#linkfire"></use>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="release">
+                            <div class="release__img">
+                                <a href="https://ltr25.lnk.to/MotivationMusic">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/_LTR25_MotivationMusicDVSG084.jpg" alt="Looptroop Rockers - Motivation Music">
+                                </a>
+                            </div>
+                            <div class="release__col1">
+                                <div class="release__artist">Looptroop Rockers</div>
+                                <div class="release__title">Motivation Music</div>
+                                <a href="https://ltr25.lnk.to/MotivationMusic" class="release__link">Listen to single</a>
+                            </div>
+                            <div class="release__col2">
+                                <div class="release__id">DVSG084</div>
+                                <div class="release__date">May 26, 2017</div>
+                                <a href="https://ltr25.lnk.to/MotivationMusic" class="release__ext-link">
                                     <svg viewBox="0 0 64 64">
                                         <use xlink:href="#linkfire"></use>
                                     </svg>
