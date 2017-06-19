840503513ede9db23a667a32d4c1e584e64eaccd
David Paulsson <david@davidpaulsson.se>
2017-06-19 07:47:30 +0200

    Add Looptroop Rockers - The Shoes of a World Traveler

--- page-ltr25.php
+++ page-ltr25.php
@@ -103,14 +103,14 @@
                         </div>
                         <div class="content-right">
                             <h1>Check back every friday for a brand new release!</h1>
-                            <h2>Next release on June 16, 2017</h2>
+                            <h2>Next release on June 23, 2017</h2>
                         </div>
                     </header>
 
                     <div class="releases-list">
                         <div class="release release--unreleased">
                             <div class="release__img">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/07_LTR25_NextReleaseJUNE16.jpg" alt="Available June 16">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/07_LTR25_NextReleaseJUNE23.jpg" alt="Available June 23">
                             </div>
                             <div class="release__col1">
                                 <div class="release__artist">Artist</div>
@@ -119,8 +119,30 @@
                             </div>
                             <div class="release__col2">
                                 <div class="release__id">DVSGXXX</div>
-                                <div class="release__date">June 16, 2017</div>
+                                <div class="release__date">June 23, 2017</div>
                                 <a href="javascript:;" class="release__ext-link">
+                                    <svg viewBox="0 0 64 64">
+                                        <use xlink:href="#linkfire"></use>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="release">
+                            <div class="release__img">
+                                <a href="https://ltr25.lnk.to/TheShoes">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/_LTR25_ShoesDVSG088.jpg" alt="Looptroop Rockers - The Shoes of a World Traveler">
+                                </a>
+                            </div>
+                            <div class="release__col1">
+                                <div class="release__artist">Looptroop Rockers</div>
+                                <div class="release__title">The Shoes of a World Traveler</div>
+                                <a href="https://ltr25.lnk.to/TheShoes" class="release__link">Listen to single</a>
+                            </div>
+                            <div class="release__col2">
+                                <div class="release__id">DVSG088</div>
+                                <div class="release__date">June 16, 2017</div>
+                                <a href="https://ltr25.lnk.to/TheShoes" class="release__ext-link">
                                     <svg viewBox="0 0 64 64">
                                         <use xlink:href="#linkfire"></use>
                                     </svg>
