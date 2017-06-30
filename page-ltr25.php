6eb5a3ef96b9cbce0824b6ed1e5fd54652dc7d12
David Paulsson <david@davidpaulsson.se>
2017-06-30 11:13:51 +0200

    Add Embudo - Blue Notes in Full Color

--- page-ltr25.php
+++ page-ltr25.php
@@ -103,14 +103,14 @@
                         </div>
                         <div class="content-right">
                             <h1>Check back every friday for a brand new release!</h1>
-                            <h2>Next release on June 30, 2017</h2>
+                            <h2>Next release on July 7, 2017</h2>
                         </div>
                     </header>
 
                     <div class="releases-list">
                         <div class="release release--unreleased">
                             <div class="release__img">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/09_LTR25_NextReleaseJUNE30.jpg" alt="Available June 30">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/10_LTR25_NextReleaseJULY7.jpg" alt="Available July 7">
                             </div>
                             <div class="release__col1">
                                 <div class="release__artist">Artist</div>
@@ -119,8 +119,30 @@
                             </div>
                             <div class="release__col2">
                                 <div class="release__id">DVSGXXX</div>
-                                <div class="release__date">June 30, 2017</div>
+                                <div class="release__date">July 7, 2017</div>
                                 <a href="javascript:;" class="release__ext-link">
+                                    <svg viewBox="0 0 64 64">
+                                        <use xlink:href="#linkfire"></use>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="release">
+                            <div class="release__img">
+                                <a href="https://ltr25.lnk.to/BlueNotesInFullColor">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/_LTR25_Embudo1DVSG089.jpg" alt="Embudo - Blue Notes in Full Color">
+                                </a>
+                            </div>
+                            <div class="release__col1">
+                                <div class="release__artist">Embudo</div>
+                                <div class="release__title">Blue Notes in Full Color</div>
+                                <a href="https://ltr25.lnk.to/BlueNotesInFullColor" class="release__link">Listen to single</a>
+                            </div>
+                            <div class="release__col2">
+                                <div class="release__id">DVSG089</div>
+                                <div class="release__date">June 30, 2017</div>
+                                <a href="https://ltr25.lnk.to/BlueNotesInFullColor" class="release__ext-link">
                                     <svg viewBox="0 0 64 64">
                                         <use xlink:href="#linkfire"></use>
                                     </svg>
