0c281705c555d0a9ef76c15457bdf56fac303a89
David Paulsson <david@davidpaulsson.se>
2017-08-18 11:43:04 +0200

    Add TCB Ha Ha (That's Right)

--- page-ltr25.php
+++ page-ltr25.php
@@ -103,14 +103,14 @@
                         </div>
                         <div class="content-right">
                             <h1>Check back every friday for a brand new release!</h1>
-                            <h2>Next release on August 18, 2017</h2>
+                            <h2>Next release on August 25, 2017</h2>
                         </div>
                     </header>
 
                     <div class="releases-list">
                         <div class="release release--unreleased">
                             <div class="release__img">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/16_LTR25_NextReleaseAUGUST18.jpg" alt="Available August 18">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/17_LTR25_NextReleaseAUGUST25.jpg" alt="Available August 25">
                             </div>
                             <div class="release__col1">
                                 <div class="release__artist">Artist</div>
@@ -119,8 +119,30 @@
                             </div>
                             <div class="release__col2">
                                 <div class="release__id">DVSGXXX</div>
-                                <div class="release__date">August 18, 2017</div>
+                                <div class="release__date">August 25, 2017</div>
                                 <a href="javascript:;" class="release__ext-link">
+                                    <svg viewBox="0 0 64 64">
+                                        <use xlink:href="#linkfire"></use>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="release">
+                            <div class="release__img">
+                                <a href="https://SonyMusic.lnk.to/CasualBrothersHaHa">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/_LTR25_HahaThatsRightDVSG096.jpg" alt="The Casual Brothers - Ha Ha (That's Right) feat. Trainspotters & MC Hans">
+                                </a>
+                            </div>
+                            <div class="release__col1">
+                                <div class="release__artist">The Casual Brothers</div>
+                                <div class="release__title">Ha Ha (That's Right) feat. Trainspotters & MC Hans</div>
+                                <a href="https://SonyMusic.lnk.to/CasualBrothersHaHa" class="release__link">Listen to single</a>
+                            </div>
+                            <div class="release__col2">
+                                <div class="release__id">DVSG096</div>
+                                <div class="release__date">August 18, 2017</div>
+                                <a href="https://SonyMusic.lnk.to/CasualBrothersHaHa" class="release__ext-link">
                                     <svg viewBox="0 0 64 64">
                                         <use xlink:href="#linkfire"></use>
                                     </svg>
