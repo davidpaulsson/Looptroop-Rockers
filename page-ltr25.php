24122a6f47920c34b31a65fcfb22de2c446bf541
David Paulsson <david@davidpaulsson.se>
2017-09-29 10:23:09 +0200

    Add The Casual Brothers - Burn feat. Elzhi

--- page-ltr25.php
+++ page-ltr25.php
@@ -103,14 +103,14 @@
                         </div>
                         <div class="content-right">
                             <h1>Check back every friday for a brand new release!</h1>
-                            <h2>Next release on September 29, 2017</h2>
+                            <h2>Next release on October 6, 2017</h2>
                         </div>
                     </header>
 
                     <div class="releases-list">
                         <div class="release release--unreleased">
                             <div class="release__img">
-                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/22_LTR25_NextReleaseSEPTEMBER29.jpg" alt="Available September 29">
+                                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/23_LTR25_NextReleaseOCTOBER6.jpg" alt="Available October 6">
                             </div>
                             <div class="release__col1">
                                 <div class="release__artist">Artist</div>
@@ -119,8 +119,30 @@
                             </div>
                             <div class="release__col2">
                                 <div class="release__id">DVSGXXX</div>
-                                <div class="release__date">September 29, 2017</div>
+                                <div class="release__date">October 6, 2017</div>
                                 <a href="javascript:;" class="release__ext-link">
+                                    <svg viewBox="0 0 64 64">
+                                        <use xlink:href="#linkfire"></use>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="release">
+                            <div class="release__img">
+                                <a href="https://ltr25.lnk.to/Burn">
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ltr25/_LTR25_BurnDVSG102.jpg" alt="The Casual Brothers - Burn feat. Elzhi">
+                                </a>
+                            </div>
+                            <div class="release__col1">
+                                <div class="release__artist">The Casual Brothers</div>
+                                <div class="release__title">Burn feat. Elzhi</div>
+                                <a href="https://ltr25.lnk.to/Burn" class="release__link">Listen to single</a>
+                            </div>
+                            <div class="release__col2">
+                                <div class="release__id">DVSG102</div>
+                                <div class="release__date">September 29, 2017</div>
+                                <a href="https://ltr25.lnk.to/Burn" class="release__ext-link">
                                     <svg viewBox="0 0 64 64">
                                         <use xlink:href="#linkfire"></use>
                                     </svg>
